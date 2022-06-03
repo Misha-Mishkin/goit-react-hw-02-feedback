ae94309f4edc5c25bc0a422ba15255d9733480c6
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-06-03 23:54:37 +0300

    Начало

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-
+import React from 'react';
+import Feedback from './components/Feedback';
 
 
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Please leave feedback</h1>
+      < Feedback />
+      <h2>
+        Statistics
+      </h2>
     </div>
   );
 };
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
-import './Feedback.css';
+import s from './Feedback.module.css';
 
+class Feedback extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+    };
 
+    render() {
+        return (
+            <div className="s.items">
+            <button type="button" className="s.item">
+                Good
+            </button>
+            <button type="button" className="s.item">
+                Neutral
+            </button>
+            <button type="button" className="s.item">
+                Bad
+            </button>
+            </div>
+        );        
+    };
+};
 
-
+export default Feedback
 
