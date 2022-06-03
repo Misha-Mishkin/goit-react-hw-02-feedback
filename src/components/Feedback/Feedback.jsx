aa730ee7f9eb3206cf0fe732b0e5722a234c222d
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-06-04 00:07:09 +0300

    Повтор загрузки на гитхаб

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,11 +2,7 @@ import React, { Component } from "react";
 import s from './Feedback.module.css';
 
 class Feedback extends Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    };
+
 
     render() {
         return (
